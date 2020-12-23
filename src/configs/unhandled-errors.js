b983893fe10e138f4a63e0ab999bb885f52a0ef3
mjb <mjb@localhost.localdomain>
2020-12-23 16:37:15 +0330

    unhandeled errors logger edited

--- src/configs/unhandled-errors.js
+++ src/configs/unhandled-errors.js
@@ -1,16 +1,8 @@
-const ADMIN_MAIL = process.env.ADMIN_MAIL;
-
-module.exports = (logger, mail) => {
-    process.on('unhandledRejection', error => {
-        mail.sendMail(ADMIN_MAIL, ADMIN_MAIL, "un handled promise rejection error", "html content");
-        logger.error(error.messages, {
-            meta: error
-        });
-    });
-    process.on('uncaughtException', error => {
-        mail.sendMail(ADMIN_MAIL, ADMIN_MAIL, "un handled promise rejection error", "html content");
-        logger.error(error.messages, {
-            meta: error
-        });
-    });
+module.exports = () => {
+  process.on('unhandledRejection', (error) => {
+    console.error(error.messages);
+  });
+  process.on('uncaughtException', (error) => {
+    console.error(error.messages);
+  });
 };
